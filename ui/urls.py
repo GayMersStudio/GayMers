fcdde95d1224a63a4158f0b17b8412d3e4db7df9
Nazar <moli@Nazars-MBP.lan>
2025-11-21 18:27:52 +0200

    50

--- ui/urls.py
+++ ui/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.default_top, name="main"),
+    path('welcome', views.welcome, name="welcome"),
+    path('sign_up', views.welcome, name="sign_up"),
     path('components/', include('django_components.urls'))
 ]
