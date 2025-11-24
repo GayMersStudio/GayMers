c3b8a2d4aea52b2b5c96ce734199adf32cd91ae8
vblacksquare <>
2025-11-24 05:05:55 +0200

    game card upgrade

--- ui/components/layouts/base/base.py
+++ ui/components/layouts/base/base.py
@@ -1,7 +1,14 @@
+from typing import Any, Optional, Mapping
 
+from django.template import Context
 from django_components import component
 
 
 @component.register("base")
 class Base(component.Component):
     template_name = "base.html"
+
+    def get_template_data(self, args: Any, kwargs: Any, slots: Any, context: Context) -> Optional[Mapping]:
+        return {
+            "blocks": kwargs.get("blocks", "none"),
+        }
